7276f07ff1aaa3e7a6ef1ff4d1048362ef9ac635
Emiliano G Solazzi Griminger <133587754+Enertopia@users.noreply.github.com>
2024-04-17 15:50:31 -0400

    Create History.jsx

---
+++ client/src/components/History.jsx
@@ -0,0 +1,14 @@
+// client/src/components/History.jsx
+import React from 'react';
+import { Box, Text } from '@chakra-ui/react';
+
+const History = () => {
+    return (
+        <Box borderWidth='1px' borderRadius='lg' overflow='hidden' p={4} m={4}>
+            <Text fontSize="xl">Transaction History</Text>
+            <Text mt={2}>Feature coming soon...</Text>
+        </Box>
+    );
+};
+
+export default History;
